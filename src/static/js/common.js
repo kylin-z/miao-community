8e57d484245590fddfeac71813d54fbae87a65ab
KailinZhou <396882574@qq.com>
2016-02-10 12:11:02 +0800

    个人面板+修复header居中

--- src/static/js/common.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by kailin on 2016/2/4.
- */
-
-define([], function () {
-    return {
-        ENTER_KEY: 13
-    };
-});
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,32 +1,6 @@
 /**
- * Created by kailin on 2016/2/4.
+ * Created by Arkay on 2015/12/10.
  */
-require.config({
-    shim:{
-        underscore:{
-            exports:'_'
-        },
-        backbone:{
-            deps:[
-                'underscore',
-                'jquery'
-            ],
-            exports:'Backbone'
-        }
-    },
-    paths: {
-        jquery:'libs/jquery-1.11.3.min',
-        underscore:'libs/underscore-min',
-        backbone:'libs/backbone-min',
-        text:'libs/text'
-    }
-});
-
-require([
-    'backbone',
-    'views/app',
-],function(Backbone,AppView){
-    Backbone.history.start();
-
-    new AppView();
-});
+define("main",["require"],function(require){
+    alert(555);
+})
